f0d1697c7c018e6ddeafd636a504c11ddf307254
yamery <jonathan.q.r.45@gmail.com>
2021-11-25 15:04:37 -0500

    100 en el bot sprint 3

--- app/src/main/java/com/tecnidev/feedmeapp/controlador/ControladorFormulario.java
+++ app/src/main/java/com/tecnidev/feedmeapp/controlador/ControladorFormulario.java
@@ -23,21 +23,56 @@ public class ControladorFormulario implements FormularioInterfaz.Controlador {
                         + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
         Pattern pattern2 = Pattern.compile("[A-Za-z]{8,}");
 
-        Pattern pattern3 = Pattern.compile("/[a-zA-Z0-9]{8}/");
-        Pattern pattern4 = Pattern.compile("/[a-zA-Z]{5}/");
+        Pattern pattern3 = Pattern.compile("/[0-9][a-zA-Z]{8}/");
+        Pattern pattern4 = Pattern.compile("^[\\\\p{Alnum}]{8,120}$");
 
         Matcher mather = pattern.matcher(formularioDTO.getEditCorreo());
         Matcher mather2 = pattern2.matcher(formularioDTO.getEditNombres());
-        Matcher mather3 = pattern2.matcher(formularioDTO.getEditUsuario());
+        Matcher mather3 = pattern4.matcher(formularioDTO.getEditUsuario());
         Matcher mather4 = pattern2.matcher(formularioDTO.getEditApellidos());
-        Matcher mather5 = pattern3.matcher(formularioDTO.getEditPassword());
+        Matcher mather5 = pattern4.matcher(formularioDTO.getEditPassword());
         Matcher mather6 = pattern3.matcher(formularioDTO.getEditNombres());
 
 
-        if((formularioDTO.getEditPassword().isEmpty())){
+        if((formularioDTO.getEditNombres().trim().isEmpty())){
+            return false;
+        }else if((formularioDTO.getEditApellidos().trim().isEmpty())){
+            return false;
+        }else if((formularioDTO.getSpSexo().trim().isEmpty())){
+            return false;
+        }else if((formularioDTO.getEditDireccion().trim().isEmpty())){
             return false;
         }
+        else if((formularioDTO.getEditCorreo().trim().isEmpty())){
+            return false;
+        }else if(!mather.find()){
+            return false;
+        }
+        else if((formularioDTO.getEditCiudad().trim().isEmpty())){
+            return false;
+        }
+        else if((formularioDTO.getEditCelular().trim().isEmpty())){
+            return false;
+        }
+        else if((formularioDTO.getEditUsuario().trim().isEmpty())){
+            return false;
+        }else if((formularioDTO.getEditPassword().trim().isEmpty())){
+            return false;
+        }else if((formularioDTO.getEditPassword().trim().length()<=5)){
+            return false;
+        }
+
         else{
+            UsuarioDto usuario = UsuarioDto.getInstance();
+            usuario.setNombre(formularioDTO.getEditNombres());
+            usuario.setApellido(formularioDTO.getEditApellidos());
+            usuario.setSexo(formularioDTO.getSpSexo());
+            usuario.setDireccion(formularioDTO.getEditDireccion());
+            usuario.setCorreo(formularioDTO.getEditCorreo());
+            usuario.setCiudad(formularioDTO.getEditCiudad());
+            usuario.setCelular(formularioDTO.getEditCelular());
+            usuario.setUsuario(formularioDTO.getEditUsuario());
+            usuario.setPassword(formularioDTO.getEditPassword());
             return true;
         }
 
@@ -48,7 +83,12 @@ public class ControladorFormulario implements FormularioInterfaz.Controlador {
 
     @Override
     public Boolean usuarioGuardarUsuario(FormularioDTO formularioDTO) {
-    return null;
+
+        if(formularioDTO!=null){
+            return true;
+        }else{
+            return false;
+        }
 
 
     }

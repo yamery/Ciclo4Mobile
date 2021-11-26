529a08b32ae938a67c828f4671031422bc1a3977
Andrengineer <andresolac@outlook.com>
2021-11-25 23:55:22 -0500

    Fix update LoginActivity, RegisterActivity

--- app/src/main/java/com/tecnidev/feedmeapp/controlador/LoginActivity.java
+++ app/src/main/java/com/tecnidev/feedmeapp/controlador/LoginActivity.java
@@ -2,20 +2,48 @@ package com.tecnidev.feedmeapp.controlador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.tecnidev.feedmeapp.interfaz.LoginIterface;
 import com.tecnidev.feedmeapp.R;
 
 public class LoginActivity extends AppCompatActivity implements LoginIterface.Controlador, View.OnClickListener {
 
-
+    // LoginActivity components
+    Button login;
+    EditText user, pass;
+    ImageButton facebook, gmail;
+    TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Components
+        user = (EditText)findViewById(R.id.editRegisterUsuario);
+        pass = (EditText)findViewById(R.id.editPassword);
+        login = (Button)findViewById(R.id.btnIngresar);
+        login.setOnClickListener(this);
+        facebook = (ImageButton)findViewById(R.id.btnFacebook);
+        gmail = (ImageButton)findViewById(R.id.btnGmail);
+
+
+        // Open up RegisterActivity on click to txtRegistrarse
+        register = (TextView)findViewById(R.id.txtRegistrarse);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerActivity = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerActivity);
+            }
+        });
     }
 
     @Override
@@ -43,6 +71,14 @@ public class LoginActivity extends AppCompatActivity implements LoginIterface.Co
 
     @Override
     public void onClick(View v) {
+
+    }
+
+    public void buttonFacebookOnClick(){
+
+    }
+
+    public void buttonGmailOnClick(){
 
     }
 }

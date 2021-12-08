edccc7caca6d9101f4550c502ddc0c9738a9f575
Andrengineer <andresolac@outlook.com>
2021-12-07 23:11:36 -0500

    MainActivity menu and fragments

---
+++ app/src/main/java/com/tecnidev/feedmeapp/controlador/HomeFragment.java
@@ -0,0 +1,30 @@
+package com.tecnidev.feedmeapp.controlador;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tecnidev.feedmeapp.R;
+
+public class HomeFragment extends Fragment {
+
+    // Required empty public constructor
+    public HomeFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
--- app/src/main/java/com/tecnidev/feedmeapp/controlador/MainActivity.java
+++ app/src/main/java/com/tecnidev/feedmeapp/controlador/MainActivity.java
@@ -1,20 +1,99 @@
 package com.tecnidev.feedmeapp.controlador;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.tecnidev.feedmeapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemReselectedListener {
+
+    // Shared preferences to use application
+    SharedPreferences sharedPreferences;
+    Boolean loggedUser;
+
+    // Fragments
+    HomeFragment homeFragment = new HomeFragment();
+    FoodsFragment foodsFragment = new FoodsFragment();
+    AddFoodFragment addFoodFragment = new AddFoodFragment();
+    UserFragment userFragment = new UserFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        // Load default fragment HomeFragment
+        loadFragment(homeFragment);
+
+        // Checking logged user
+        sharedPreferences = this.getPreferences(Context.MODE_PRIVATE);
+        loggedUser = sharedPreferences.getBoolean(String.valueOf(R.bool.userLogged), false);
+
+        // Load LoginActivity if not logged user yet
+        if (!loggedUser){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    // Top menu behaviour
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.user, menu);
+        return true;
+    }
+
+    // Top menu item selected
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        // Logout and finish App
+        if (id==R.id.logout){
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(String.valueOf(R.bool.userLogged), "false");
+            editor.apply();
+            Toast.makeText(this, "Saliendo...", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        return true;
+    }
+
+    // Swap selected fragment into RecyclerView
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.fragmentView, fragment);
+        transaction.commit();
+    }
+
+    @Override
+    public void onNavigationItemReselected(@NonNull MenuItem item) {
+        // Swap fragment according to clicked menu item
+        switch (item.getItemId()){
+            case R.id.home:
+                loadFragment(homeFragment);
+                break;
+
+            case R.id.list:
+                loadFragment(foodsFragment);
+                break;
+
+            case R.id.add:
+                loadFragment(addFoodFragment);
+                break;
+
+            case R.id.userMenu:
+                loadFragment(userFragment);
+                break;
+        }
     }
 }
